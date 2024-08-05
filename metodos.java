0ca2eabe559f4228f1a9f9b059793bb771f693cc
Gabriel <javarealfan@gmail.com>
2024-08-05 09:39:53 -0400

    Java-bootcamp-dio
    Aprendendo java, começando por métodos

---
+++ metodos.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class metodos {
+
+    public static void main(String[] args){
+
+       String primeiroNome = "Gabriel";
+       String segundoNome = "Arruda";
+       String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+       System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+
+        return "Resultado do método: " + primeiroNome.concat("").concat(segundoNome);
+    }
+    
+    
+}
