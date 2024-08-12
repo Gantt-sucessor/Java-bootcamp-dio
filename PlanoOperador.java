f140fe99263fb4b63ee6a54de64aa4411c2df781
Gabriel <javarealfan@gmail.com>
2024-08-12 10:23:39 -0400

    Switch case sem o break
    Vendo como pode ser usado switch case sem o break

---
+++ PlanoOperador.java
@@ -0,0 +1,19 @@
+public class PlanoOperador {
+    
+    public static void main(String[] args) {
+        
+        String plano = "T";
+
+        switch (plano) {
+            case "T": {
+                System.out.printf("5gb youtube");
+            }
+            case "M": {
+                System.out.printf(" Whats e Instagram grátis");
+            }
+            case "B": {
+                System.out.printf(" e mais 100 minutos de ligação");
+            }
+        }
+    }
+}
